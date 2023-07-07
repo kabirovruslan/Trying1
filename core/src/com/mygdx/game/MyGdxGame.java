190e5572d004ccaec64d015536420ce496f95d8f
kabirovruslan <kabrus2005@mail.ru>
2023-07-07 17:56:10 +0300

    add homeMechanism

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,24 +10,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.Timer;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture background, home;
+	Texture blue,red,background, home;
 	Button goToBuyHome;
 	Sprite sprite;
 	int x,y;
 	boolean touchHome,touchSquare,pressedHome;
-
-
+	Square[][] mas;
+	boolean [][] isEmpty;
+	int time;
 
 	public static int SRC_WIDTH, SRC_HEIGHT;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		background = new Texture("background.jpg");
+		blue = new Texture("blue.png");
+		background = new Texture("background.png");
 		home = new Texture("home.png");
+		red = new Texture("red.png");
 
 		goToBuyHome = new Button();
 
@@ -40,12 +44,51 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		x = SRC_WIDTH - 240;
 		y = 0;
+
+		mas = new Square[18][32];
+		isEmpty = new boolean[18][32];
+		for (int i = 0; i<18;i++){
+			for(int j = 0;j<32;j++){
+				mas[i][j] = new Square((SRC_WIDTH-1920)/2+j*60,(17-i)*60);
+				isEmpty[i][j] = false;
+			}
+		}
+
+
+
 	}
 
 	@Override
 	public void render() {
 		batch.begin();
-		batch.draw(background, 0, 0, SRC_WIDTH, SRC_HEIGHT);
+		for (int i = 0; i<18;i++) {
+			for (int j = 0; j < 32; j++) {
+				if (mas[i][j].isEmpty == isEmpty[i][j]){
+					mas[i][j].isEmpty = false;
+				}
+			}
+		}
+		batch.draw(blue,0,0,SRC_WIDTH,SRC_HEIGHT);
+		batch.draw(background,(SRC_WIDTH-1920)/2 , (SRC_HEIGHT-1080)/2, 1920, 1080);
+		for (int i = 17; i >=0; i--) {
+			for (int j = 0; j < 32; j++) {
+				if ( !mas[i][j].isEmpty && (j%4==0) && (i+3)%4==0 ){
+					batch.draw(home,(SRC_WIDTH-1920)/2+j*60,(17-i)*60);
+					mas[i][j].isEmpty = true;
+					mas[17-i][j].isEmpty = true;
+					mas[i][j+1].isEmpty = true;
+					mas[17-i][j+1].isEmpty = true;
+
+					isEmpty[i][j] = true;
+					isEmpty[17-i][j] = true;
+					isEmpty[i][j+1] = true;
+					isEmpty[17-i][j+1] = true;
+				}
+			}
+			/*if (i==16) break;*/
+		}
+
+
 		Gdx.input.setInputProcessor(new InputAdapter(){
 			@Override
 			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
@@ -57,9 +100,6 @@ public class MyGdxGame extends ApplicationAdapter {
 
 					return true;
 			}
-
-
-
 			@Override
 			public boolean touchDragged(int screenX, int screenY, int pointer) {
 				if (screenX>x && screenX<x+240 &&screenY>SRC_HEIGHT-y-240 && screenY<SRC_HEIGHT-y){
--- core/src/com/mygdx/game/Square.java
+++ core/src/com/mygdx/game/Square.java
@@ -3,9 +3,12 @@ package com.mygdx.game;
 public class Square {
     int width,height;
     int x,y;
+    boolean isEmpty = false;
 
 
-    public Square() {
+    public Square(int x,int y) {
+        this.x = x;
+        this.y = y;
 
     }
 }
